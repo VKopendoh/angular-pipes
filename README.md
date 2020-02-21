6b6fe1268d23b2ccdc2a8f8aedecf0979f0e1b0b
vkopendoh <vkopendoh@bk.ru>
2020-02-21 10:16:06 +0300

    Pipe examples

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # AppPipes
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.8.
-
+Pipes info you can find in angular.io in left menu choose API and then in TYPE filter: Pipes
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,73 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable} from 'rxjs';
+
+export interface Post {
+  title: string;
+  text: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'appPipes';
+export class AppComponent implements OnInit {
+  p: Promise<string> = new Promise<string>(resolve => {
+    setTimeout(() => {
+      resolve('Promise Resolved');
+    }, 4000);
+  });
+
+  date2: Observable<Date> = new Observable<Date>(obs => {
+    setInterval(() => {
+      obs.next(new Date());
+    }, 1000);
+  });
+  date3$: Observable<Date> = new Observable<Date>(obs => {
+    setInterval(() => {
+      obs.next(new Date());
+    }, 1000);
+  });
+
+  date3: Date;
+
+  e: number = Math.E;
+
+  str = 'hello world';
+
+  date: Date = new Date();
+  float = 0.42;
+
+  obj = {
+    a: 1,
+    b: {
+      c: 2,
+      d: {
+        e: 4,
+        f: 10
+      }
+    }
+  };
+
+  posts: Post[] = [
+    {title: 'Beer', text: 'Take some beer'},
+    {title: 'Water', text: 'Drink some water'},
+    {title: 'Wine', text: 'Get wine bro'}
+  ];
+  search = '';
+  searchField = 'title';
+
+  addPost() {
+    this.posts.unshift({
+      title: 'Angular 8',
+      text: 'super duper course'
+    });
+  }
+
+// classic use Observable
+  ngOnInit(): void {
+    this.date3$.subscribe(date => {
+      this.date3 = date;
+    });
+  }
 }
